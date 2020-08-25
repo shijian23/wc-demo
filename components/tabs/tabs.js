3d1ec272bce13fd57f8f8222703e5f20914d3d94
shijian <418554980@qq.com>
2020-08-25 18:04:29 +0800

    自定义tab

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -1,5 +1,46 @@
 Component({
-    data: {},
-    properties: {},
-    methods: {},
+    data: {
+        tabList: [
+            {
+                id: 1,
+                name: "全部",
+            },
+            {
+                id: 2,
+                name: "分类",
+            },
+            {
+                id: 3,
+                name: "其他",
+            },
+            {
+                id: 4,
+                name: "更多",
+            },
+        ],
+    },
+    pageLifetimes: {
+        show: function () {
+            console.log(this.properties.currentIndex);
+            // 页面被展示
+        },
+        hide: function () {
+            // 页面被隐藏
+        },
+        resize: function (size) {
+            // 页面尺寸变化
+        },
+    },
+    properties: {
+        currentIndex: {
+            type: Number,
+            value: 0,
+        },
+    },
+    methods: {
+        clickTab(e) {
+            console.log(this.properties.currentIndex);
+            this.triggerEvent("myTab", e);
+        },
+    },
 });
--- pages/cityClient/cityKnow/cityKnowInfo/cityKnowInfo.js
+++ pages/cityClient/cityKnow/cityKnowInfo/cityKnowInfo.js
@@ -1,1 +1,38 @@
-Page({})
+Page({
+    data: {
+        backClass: [
+            {
+                id: 1,
+                name: "red",
+                class: "red",
+            },
+            {
+                id: 2,
+                name: "blue",
+                class: "blue",
+            }
+        ],
+        currentItemId: 0,
+        currentIndex: 0,
+    },
+    swiperChange: function (e) {
+        console.log(e);
+        var currentItemId = e.detail.currentItemId;
+        this.setData({
+            currentItemId: currentItemId,
+        });
+    },
+    clickChange: function (e) {
+        console.log(e);
+        var itemId = e.currentTarget.dataset.itemId;
+        this.setData({
+            currentItemId: itemId,
+        });
+    },
+    myTab(e) {
+        console.log(e);
+        this.setData({
+            currentIndex: e.detail.currentTarget.dataset.index,
+        });
+    },
+});
